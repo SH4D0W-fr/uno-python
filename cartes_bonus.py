52081efb8d7edd3c7aac3877a534d7ae9c17e80c
SHADOW <contact@shadow-dev.fr>
2026-09-08 09:18:01 +0200

    feat: Création des cartes bonus

---
+++ cartes_bonus.py
@@ -0,0 +1,41 @@
+cartes_bonus = [] # Liste stockant toutes les cartes bonus
+
+class CartesBonus:
+    def creer_ids(self, couleur:list, valeur:str, nombre:int, sansCouleur:bool):
+        if not sansCouleur:
+            for i in range(len(couleur) - 1):
+                for k in range(nombre):
+                    id_carte = (valeur, couleur[i], str(k+1))
+                    cartes_bonus.append(id_carte) # Exemple : "inversionrouge1", "plus2vert2"
+        else:
+            for k in range(nombre):
+                id_carte = (valeur, str(k+1))
+                cartes_bonus.append(id_carte) # Exemple : "plus41", "plus2vert2"
+
+    def __init__(self, valeur:str, nombre:int, sansCouleur:bool):
+        """
+        valeur:str : Valeur de la carte (plus2, inversion, plus4...)
+        nombre:int : Nombre de carte par couleur
+        sansCouleur:bool : Est-ce que la carte n'a pas de couleur ?
+        """
+        self.col = ["rouge", "bleu", "vert", "jaune"]
+        self.val = valeur
+        self.n = nombre
+        self.wcol = sansCouleur
+        # On créé un ID par carte
+        self.creer_ids(self.col, self.val, self.n, self.wcol)
+
+### CREATION DES CARTES BONUS ###
+plus2 = CartesBonus("plus2", 2, False)
+inversion = CartesBonus("inversion", 2, False)
+passer = CartesBonus("passer", 2, False)
+joker = CartesBonus("joker", 4, True)
+plus4 = CartesBonus("plus4", 4, True)
+print(cartes_bonus)
+
+# Tests unitaires
+if __name__ == "__main__":
+    # plus2 = CartesBonus("plus2", 2, False)
+    # plus4 = CartesBonus("plus4", 2, True)
+    # print(cartes_bonus)
+    pass
