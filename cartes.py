d6501a0d7912c732f54375d52a62068c2dffd3f4
SHADOW <contact@shadow-dev.fr>
2026-09-08 13:03:09 +0200

    refact: Remplacement de l'ID couleur en int par str

--- cartes.py
+++ cartes.py
@@ -1,39 +1,26 @@
-class CarteNombre:
+class CartesNombre:
 
-    def setCartes(self):
-        cartes = [(0, 0, 0), (0, 1, 0), (0, 2, 0), (0, 3, 0)]
+    def creerCartes(self):
+        cartes = [(0, "rouge", 0), (0, "jaune", 0), (0, "vert", 0), (0, "bleu", 0)] # Création manuelle des cartes 0 de chaque couleur
+        couleurs = ["rouge", "jaune", "vert", "bleu"]
         for i in range(9):
             for k in range(4):
-                for id in range(2):
-                    carte = (i+1, k, id+1)
+                for num in range(2):
+                    carte = (i+1, couleurs[k], num+1) # Format du tuple : (numéro:int, couleur:str, id:int)
                     cartes.append(carte)
         return(cartes)
 
 
-    ## 0 = rouge
-    ## 1 = jaune
-    ## 2 = vert
-    ## 3 = bleu
-
-    def __init__(self, nombre, couleur, identifiant):
-        self.cartes = self.setCartes()
-        self.col = couleur #4 couleurs disponible (rouge,  bleu, jaune, vert)
-        self.num = nombre #nombre de 0 à 9
-        self.id = identifiant #identifiant de la carte de 1 à 2
-
-    def getAttributs(self):
-        return(self.num, self.col, self.id)
+    def __init__(self):
+        self.cartes = self.creerCartes()
 
     def getCartes(self):
         return(self.cartes)
 
 
+cartes = CartesNombre().getCartes() # Liste importée par main.py
+
 
 if __name__ == "__main__":
-    c1 = CarteNombre("jaune", 4, 1)
-    print(CarteNombre.getAttributs(c1))
-    print(CarteNombre.setCartes())
-    a = 0
-    for i in CarteNombre.setCartes():
-        a = a + 1
-        print(a)
+    print(cartes)
+    print(len(cartes))
