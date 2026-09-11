efa2c66537cf6d11787eb26b6546a8e97742d077
Pcportable <vipelevre@gmail.com>
2026-09-11 10:51:46 +0200

    fix typographie, syntax défausse, pioche

--- defausse.py
+++ defausse.py
@@ -2,14 +2,14 @@ import random
 
 pile = []
 
-class défausse:
+class Défausse:
 
     def pileDéfausse(self, carte):
         pile.append((0, "rouge", 0))    ### IMPLEMETNER LE VRAI SYSTEME
 
 
     def __init__(self):
-        self.défausse = défausse.pileDéfausse
+        self.défausse = Défausse.pileDéfausse
 
     def mélangerDéfausse(self):
         return random.shuffle(self.défause)
--- pioche.py
+++ pioche.py
@@ -1,25 +1,26 @@
 import random
 
+class Pioche:
 
-def piocher(pioche, defausse):
-        #il faut que "pioche et défausse" soient des listes de cartes dcp?
-    if len(pioche) == 0:
-        #transfère :
-        pioche.extend(defausse)
-        #pour vider :
-        defausse.clear()
-        #et pour mélanger :
-        random.shuffle(pioche)
-    return pioche.pop()
+    def piocher(pioche, defausse):
+            #il faut que "pioche et défausse" soient des listes de cartes dcp?
+        if len(pioche) == 0:
+            #transfère :
+            pioche.extend(defausse)
+            #pour vider :
+            defausse.clear()
+            #et pour mélanger :
+            random.shuffle(pioche)
+        return pioche.pop()
 
 
-def piocher_plusieurs(pioche, defausse, nombre):
-    cartes = []
-    for i in range(nombre):
-        #dcp nombre de carte c un entier mais il vient de où?
-        carte = piocher(pioche, defausse)
-        cartes.append(carte)
-    return cartes
+    def piocher_plusieurs(pioche, defausse, nombre):
+        cartes = []
+        for i in range(nombre):
+            #dcp nombre de carte c un entier mais il vient de où?
+            carte = piocher(pioche, defausse)
+            cartes.append(carte)
+        return cartes
 
 
 
